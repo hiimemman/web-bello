a76de87052157599ade69a51b47e2c728104ea3a
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-11 12:18:46 +0800

    Test again

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['IDUSER'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Welcome to Web-Bello Test</title>
+    <title>Home - Welcome to Web-Bello Test2</title>
     <link rel="stylesheet" href="../style.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
 </head>
