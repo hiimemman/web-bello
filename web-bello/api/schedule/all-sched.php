8935992d2f6b0f73a6d0bd7d744cf2f047d87860
Olarvs <mauuolarvs@gmail.com>
2023-06-12 20:24:28 +0800

    Updating sched

--- web-bello/api/schedule/all-sched.php
+++ web-bello/api/schedule/all-sched.php
@@ -2,26 +2,7 @@
 include_once("../../connections/connection.php");
 $con = connection();
 
-// Check if form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Retrieve form data
-  $title = $_POST['title'];
-  $start_date = $_POST['start_date'];
-  $end_date = $_POST['end_date'];
-
-  // Insert the event into the database
-  $sql = "INSERT INTO tbl_sched (title, start_date, end_date) VALUES ('$title', '$start_date', '$end_date')";
-  if (mysqli_query($con, $sql)) {
-    $response = array("status" => "success", "message" => "Event scheduled successfully.");
-  } else {
-    $response = array("status" => "error", "message" => "Error: " . mysqli_error($con));
-  }
-
-  // Return the response as JSON
-  echo json_encode($response);
-}
-
-// Fetch all events from the database
+// Select all events from the database
 $sql = "SELECT * FROM tbl_sched";
 $result = mysqli_query($con, $sql);
 
@@ -39,11 +20,9 @@ if (mysqli_num_rows($result) > 0) {
 
     $events[] = $event;
   }
-
-  // Return the events as JSON response
-  echo json_encode($events);
-} else {
-  // No events found
-  echo json_encode([]);
 }
+
+// Return the events as JSON response
+header('Content-Type: application/json'); // Set the content type header
+echo json_encode($events);
 ?>
