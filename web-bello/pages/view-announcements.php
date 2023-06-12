2bf5750179490060ef97c18d3af510f24390ff0e
joegrayson <kingjoelunas@gmail.com>
2023-06-12 18:46:03 +0800

    Made changes on view-announcements.php

--- web-bello/pages/view-announcements.php
+++ web-bello/pages/view-announcements.php
@@ -53,142 +53,56 @@ require_once('../components/navbar.php')
         <!-- End of Intro Header -->
     </header>
 
-    <!-- New Content Section -->
-    <section>
-        <div class="max-w-screen-xl mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-            <div class="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:gap-x-16">
-                <div class="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right lg:text-left">
-                    <h2 class="text-3xl font-bold sm:text-4xl">
-                        Community Reminder
-                    </h2>
+    <!-- Announcement Section -->
 
-                    <p class="mt-4 text-gray-700 tracking-wide text-md">
-                        Dear homeowners, we want to remind you of your crucial role in fulfilling your financial
-                        responsibilities to the association. Please review your account to ensure all dues are up to
-                        date. Maintaining a healthy homeowner's association requires your active participation. Click
-                        <span class="font-medium text-gray-900">'Review Account'</span> below to check your balance and
-                        payment status. Together, let's create a vibrant community. Thank you for your cooperation!
-                    </p>
-
-                    <a class="mt-8 group relative inline-flex items-center overflow-hidden rounded bg-slate-800 px-14 py-4 text-white focus:outline-none focus:ring active:bg-indigo-500"
-                        href="#">
-                        <span class="absolute -end-full transition-all group-hover:end-4">
-                            <svg class="h-5 w-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                            </svg>
-                        </span>
-
-                        <span class="text-sm font-medium tracking-wider transition-all group-hover:me-4">
-                            REVIEW ACCOUNT
-                        </span>
-                    </a>
+    <section class="border-t-4 border-sky-300">
+        <div class="container mx-auto grid gap-8 lg:grid-cols-1 p-6 mt-10">
+            <article
+                class="p-12 mx-20 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <div class="flex justify-between items-center mb-5 text-gray-500">
+                    <span
+                        class="bg-purple-100 text-purple-700 text-md font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                        Announcement
+                    </span>
+                    <span class="text-sm">2023-06-09 01:36:10</span>
                 </div>
-
-                <!-- Grid Boxes -->
-
-                <div class="grid grid-cols-2 gap-4 sm:grid-cols-2">
-                    <a class="block rounded-xl border border-gray-300 p-4 shadow-sm hover:shadow-xl transition-shadow focus:outline-none focus:ring grid-box"
-                        href="#">
-                        <span class="flex items-center justify-center rounded-lg p-3">
-                            <svg class="h-12 w-12" fill="none" stroke="currentColor" stroke-width="1.5"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z">
-                                </path>
-                            </svg>
-                        </span>
-
-                        <h2 class="mt-2 font-medium tracking-wider text-center">
-                            PAY BILLS
-                        </h2>
-                    </a>
-
-                    <a class="block rounded-xl border border-gray-300 p-4 shadow-sm hover:shadow-xl transition-shadow focus:outline-none focus:ring grid-box"
-                        href="#">
-                        <span class="flex items-center justify-center rounded-lg p-3">
-                            <svg class="h-12 w-12" fill="none" stroke="currentColor" stroke-width="1.5"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10">
-                                </path>
-                            </svg>
-                        </span>
-
-                        <h2 class="mt-2 font-medium tracking-wider text-center">
-                            UPDATE PROFILE
-                        </h2>
-                    </a>
-
-                    <a class="block rounded-xl border border-gray-300 p-4 shadow-sm hover:shadow-xl transition-shadow focus:outline-none focus:ring grid-box"
-                        href="#">
-                        <span class="flex items-center justify-center rounded-lg p-3">
-                            <svg class="h-12 w-12" fill="none" stroke="currentColor" stroke-width="1.5"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46">
-                                </path>
-                            </svg>
-                        </span>
-
-                        <h2 class="mt-2 font-medium tracking-wider text-center">
-                            VIEW ANNOUNCEMENTS
-                        </h2>
-                    </a>
-
-                    <a class="block rounded-xl border border-gray-300 p-4 shadow-sm hover:shadow-xl transition-shadow focus:outline-none focus:ring grid-box"
-                        href="#">
-                        <span class="flex items-center justify-center rounded-lg p-3">
-                            <svg class="h-12 w-12" fill="none" stroke="currentColor" stroke-width="1.5"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z">
-                                </path>
-                            </svg>
-                        </span>
-
-                        <h2 class="mt-2 font-medium tracking-wider text-center">
-                            VIEW EVENTS
-                        </h2>
-                    </a>
-
-                    <a class="block rounded-xl border border-gray-300 p-4 shadow-sm hover:shadow-xl transition-shadow focus:outline-none focus:ring grid-box"
-                        href="#">
-                        <span class="flex items-center justify-center rounded-lg p-3">
-                            <svg class="h-12 w-12" fill="none" stroke="currentColor" stroke-width="1.5"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z">
-                                </path>
-                            </svg>
-                        </span>
-
-                        <h2 class="mt-2 font-medium tracking-wider text-center">
-                            VIEW FORUMS
-                        </h2>
-                    </a>
-
-                    <a class="block rounded-xl border border-gray-300 p-4 shadow-sm hover:shadow-xl transition-shadow focus:outline-none focus:ring grid-box"
-                        href="#">
-                        <span class="flex items-center justify-center rounded-lg p-3">
-                            <svg class="h-12 w-12" fill="none" stroke="currentColor" stroke-width="1.5"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12">
-                                </path>
-                            </svg>
-                        </span>
-
-                        <h2 class="mt-2 font-medium tracking-wider text-center">
-                            VIEW SCHEDULES
-                        </h2>
-                    </a>
+                <div class="flex justify-between items-center">
+                    <div class="flex items-center space-x-4">
+                        <img class="w-7 h-7 rounded-full"
+                            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                            alt="Bonnie Green avatar" />
+                        <span class="font-medium dark:text-white"> John Doe </span>
+                    </div>
                 </div>
-            </div>
+                <h2 class="mt-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Our first project with React
+                </h2>
+                <p id="announcementText" class="font-normal line-clamp-2 text-lg text-gray-500 dark:text-gray-400">
+                    Static websites are now used to bootstrap lots of websites and are
+                    becoming the basis for a variety of tools that even influence both
+                    web designers and developers influence both web designers and
+                    developers. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit. Architecto delectus cum velit dignissimos minus nesciunt
+                    numquam, magni laborum sapiente debitis amet, provident fugiat odio
+                    cupiditate blanditiis eaque illo voluptatum quis! Lorem ipsum dolor
+                    sit amet consectetur adipisicing elit. Corrupti quia assumenda
+                    provident omnis dicta velit quibusdam reiciendis rerum, perspiciatis
+                    nostrum molestias at aut. Minima quasi saepe maxime quas quibusdam
+                    ipsa!
+                </p>
+                <a id="readMoreLink" href="#"
+                    class="mb-5 inline-flex items-center font-medium text-lg text-primary-600 dark:text-primary-500 hover:underline">
+                    See more...
+                </a>
+                <div class="flex justify-center">
+                    <img alt="image" src="https://via.placeholder.com/1920x1080"
+                        class="mt-8 h-auto w-auto object-cover" />
+                </div>
+            </article>
         </div>
     </section>
-    <!-- End of New Content Section-->
+
+    <!-- End of Announcement Section -->
 
     <!-- Footer -->
     <footer class="bg-gray-100">
