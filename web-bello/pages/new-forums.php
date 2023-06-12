274d87f9445b6de16f66d7f856868af8fe4a211b
Olarvs <mauuolarvs@gmail.com>
2023-06-12 13:09:40 +0800

    Updating new forums

--- web-bello/pages/new-forums.php
+++ web-bello/pages/new-forums.php
@@ -49,7 +49,7 @@ if (!isset($_SESSION['IDUSER'])) {
 
 <body class="bg-white dark:bg-gray-900">
 <div class="px-4 mt-4 shadow rounded-lg bg-white dark:bg-dark-second">
-                <div class="p-2 border-b border-gray-300 dark:border-dark-third flex mx-auto space-x-4">
+                <div class="p-2 border-b border-gray-300 dark:border-dark-third flex mr-auto space-x-4">
                 <!-- LIST POST -->
                 <!-- <h1 class="text-gray-500 dark:text-gray-400">This is result id<?php //echo $_SESSION['IDUSER']; ?></h1> -->
                 <main class="px-6 sm:ml-64 h-auto" id="forumMain">
