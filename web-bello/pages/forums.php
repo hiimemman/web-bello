72d740b29aa63a5abbdf41f727bc7f96b257588c
Olarvs <mauuolarvs@gmail.com>
2023-06-12 11:57:26 +0800

    New layout

--- web-bello/pages/forums.php
+++ web-bello/pages/forums.php
@@ -369,24 +369,25 @@ if (!isset($_SESSION['IDUSER'])) {
             }
             content += `  <div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 mb-5">
   <div class="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4 mb-4">
-    <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
-      <footer class="flex justify-between items-center mb-2">
+  <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
+    <footer class="flex justify-between items-center mb-2">
         <div class="flex items-center">
-          <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-            <img class="mr-2 w-6 h-6 rounded-full" src="` + forum.image_url + `" alt="` + forum.editor_email + `">` + forum.editor_email + `
-            <img class="h-auto max-w-xs rounded-lg m-2 forum-image" src="${forum.image_url}" alt="image description">
+            <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                <img class="mr-2 w-6 h-6 rounded-full" src="${forum.image_url}" alt="${forum.editor_email}">
+                ${forum.editor_email}
             </p>
-          `+categoryBadge+`
+            ${categoryBadge}
         </div>
         <p class="text-sm text-gray-600 dark:text-gray-400 mr-2">
-          <time pubdate datetime="` + forum.created_at + `" title="` + forum.created_at + `">` + forum.created_at + `</time>
+            <time pubdate datetime="${forum.created_at}" title="${forum.created_at}">${forum.created_at}</time>
         </p>
-      </footer>
-    </article>
+    </footer>
+</article>
 
-    <h2 class="text-gray-500 dark:text-gray-400"><strong>` + forum.title + `</strong></h2>
-    <p class="text-gray-500 dark:text-gray-400">` + forum.message_body + `</p>
-    <img class="h-auto max-w-xs rounded-lg m-2 forum-image" src="` + forum.image_url + `" alt="image description">
+<h2 class="text-gray-500 dark:text-gray-400"><strong>${forum.title}</strong></h2>
+<p class="text-gray-500 dark:text-gray-400">${forum.message_body}</p>
+<img class="h-auto max-w-xs rounded-lg m-2 forum-image" src="${forum.image_url}" alt="image description">
+
     <div class="flex items-center">
       <button class="flex items-center px-3 py-2 bg-blue-500 text-white rounded-full focus:outline-none" id="btnLike`+forum.id+`" onClick="likedButtonClicked(`+forum.id+`)">
         👍
