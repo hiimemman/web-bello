d3e917366ad0a2c4f04d42bdaed05a8faf52dd46
Olarvs <mauuolarvs@gmail.com>
2023-06-12 14:50:10 +0800

    New forums

--- web-bello/pages/new-forums.php
+++ web-bello/pages/new-forums.php
@@ -153,7 +153,7 @@ content += `
 
     <!-- POST IMAGE -->
     <div class="py-2">
-        <img class="mx-auto max-w-full" style="max-width: 100%; height: auto;" src="` + forum.image_url + `">
+        <img class="w-full" src="` + forum.image_url + `">
     </div>
     <!-- END POST IMAGE -->
 
