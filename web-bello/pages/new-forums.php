e7497ba36268b60ec1cb83f066bf94329a11e8f0
Olarvs <mauuolarvs@gmail.com>
2023-06-12 13:43:39 +0800

    Updating New Forum

--- web-bello/pages/new-forums.php
+++ web-bello/pages/new-forums.php
@@ -66,7 +66,7 @@ if (!isset($_SESSION['IDUSER'])) {
     <div class="flex items-center justify-between px-4 py-2">
         <div class="flex space-x-2 items-center">
             <div class="relative">
-                <img src="./images/avt-2.jpg" alt="Profile picture" class="w-10 h-10 rounded-full">
+                <img src="" alt="Profile picture" class="w-10 h-10 rounded-full">
                 <span class="bg-green-500 w-3 h-3 rounded-full absolute right-0 top-3/4 border-white border-2"></span>
             </div>
             <div>
@@ -293,83 +293,183 @@ if (!isset($_SESSION['IDUSER'])) {
 
 // CONTENTS FORM            
 content += `  
-<div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700 mb-5">
-  <div class="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-4 mb-4">
-    <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
-      <footer class="flex justify-between items-center mb-2">
-        <div class="flex items-center">
-          <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-            <img class="mr-2 w-6 h-6 rounded-full" src="` + forum.image_url + `" alt="` + forum.editor_email + `">` + forum.editor_email + `
-          </p>
-          `+categoryBadge+`
+<!-- POST -->
+<div class="shadow bg-white dark:bg-dark-second dark:text-dark-txt mt-4 rounded-lg">
+    <!-- POST AUTHOR -->
+    <div class="flex items-center justify-between px-4 py-2">
+        <div class="flex space-x-2 items-center">
+            <div class="relative">
+                <img src="" alt="Profile picture" class="w-10 h-10 rounded-full">
+                <span class="bg-green-500 w-3 h-3 rounded-full absolute right-0 top-3/4 border-white border-2"></span>
+            </div>
+            <div>
+                <div class="font-semibold">
+                    John Doe
+                </div>
+                <span class="text-sm text-gray-500">38m</span>
+            </div>
         </div>
-        <p class="text-sm text-gray-600 dark:text-gray-400 mr-2">
-          <time pubdate datetime="` + forum.created_at + `" title="` + forum.created_at + `">` + forum.created_at + `</time>
-        </p>
-      </footer>
-    </article>
+        <div class="w-8 h-8 grid place-items-center text-xl text-gray-500 hover:bg-gray-200 dark:text-dark-txt dark:hover:bg-dark-third rounded-full cursor-pointer">
+            <i class='bx bx-dots-horizontal-rounded'></i>
+        </div>
+    </div>
+    <!-- END POST AUTHOR -->
 
-    <h2 class="text-gray-500 dark:text-gray-400"><strong>` + forum.title + `</strong></h2>
-    <p class="text-gray-500 dark:text-gray-400">` + forum.message_body + `</p>
-    <img class="h-auto max-w-xs rounded-lg m-2 forum-image" src="` + forum.image_url + `" alt="image description">
-    <div class="flex items-center">
-      <button class="flex items-center px-3 py-2 bg-blue-500 text-white rounded-full focus:outline-none" id="btnLike`+forum.id+`" onClick="likedButtonClicked(`+forum.id+`)">
-        👍
-        <span id="likeCount`+forum.id+`" class="text-sm">`+forum.like_count+`</span>
-      </button>
+    <!-- POST CONTENT -->
+    <div class="text-justify px-4 py-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, autem earum cum ullam odio, molestias maxime aperiam in id aspernatur vel ratione odit molestiae minus ipsa obcaecati quia! Doloribus, illum.
     </div>
-    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
-    <h2 class="text-gray-500 dark:text-gray-400 m-1"><strong>Comments</h2>
-  </div>
-  <form action="https://web-bello.online/web-bello/api/comment/add-comment-per-forum.php" method="POST">
-    <div id="commentSection`+forum.id+`"></div>
-    <input type="text" style="display: none;" name="user_id" value="<?php echo $result[0]['id']; ?>">
-    <input type="text" style="display: none;" name="forum_id" value="`+forum.id+`">
-    <input type="text" style="display: none;" name="user_email" value="<?php echo $result[0]['email']; ?>">
-    <input type="text" style="display: none;" name="user_full_name" value="<?php echo $result[0]['firstname'] . ' ' . $result[0]['lastname']; ?>">
-    <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-      <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-        <label for="comment_text" class="sr-only">Your comment</label>
-        <textarea id="comment_text" name="comment_text" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 font-sans" placeholder="Write a comment..." required></textarea>
-      </div>
-      <div class="mt-2" id="imageHolder`+forum.id+`"></div>
-      <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-        <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-          Post comment
-        </button>
-        <div class="flex pl-0 space-x-1 sm:pl-2">
-          <input style="display: none;" id="imageUrl`+forum.id+`" type="file" onchange="changeProfile()" />
-          <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600" onClick="triggerInputClick(`+forum.id+`)">
-            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path>
-            </svg>
-            <span class="sr-only">Upload image</span>
-          </button>
+    <!-- END POST CONTENT -->
+
+    <!-- POST IMAGE -->
+    <div class="py-2">
+        <img src="./images/post.png" alt="Post image">
+    </div>
+    <!-- END POST IMAGE -->
+
+    <!-- POST REACT -->
+    <div class="px-4 py-2">
+        <div class="flex items-center justify-between">
+            <div class="flex flex-row-reverse items-center">
+                <span class="ml-2 text-gray-500 dark:text-dark-txt">999</span>
+                <span class="rounded-full grid place-items-center text-2xl -ml-1 text-red-800">
+                    <i class='bx bxs-angry'></i>
+                </span>
+                <span class="rounded-full grid place-items-center text-2xl -ml-1 text-red-500">
+                    <i class='bx bxs-heart-circle'></i>
+                </span>
+                <span class="rounded-full grid place-items-center text-2xl -ml-1 text-yellow-500">
+                    <i class='bx bx-happy-alt'></i>
+                </span>
+            </div>
+            <div class="text-gray-500 dark:text-dark-txt">
+                <span>90 comments</span>
+                <span>66 Shares</span>
+            </div>
         </div>
-      </div>
     </div>
-  </form>
+    <!-- END POST REACT -->
+
+    <!-- POST ACTION -->
+    <div class="py-2 px-4">
+        <div class="border border-gray-200 dark:border-dark-third border-l-0 border-r-0 py-1">
+            <div class="flex space-x-2">
+                <div class="w-1/3 flex space-x-2 justify-center items-center hover:bg-gray-100 dark:hover:bg-dark-third text-xl py-2 rounded-lg cursor-pointer text-gray-500 dark:text-dark-txt">
+                    <i class='bx bx-like'></i>
+                    <span class="text-sm font-semibold">Like</span>
+                </div>
+                <div class="w-1/3 flex space-x-2 justify-center items-center hover:bg-gray-100 dark:hover:bg-dark-third text-xl py-2 rounded-lg cursor-pointer text-gray-500 dark:text-dark-txt">
+                    <i class='bx bx-comment'></i>
+                    <span class="text-sm font-semibold">Comment</span>
+                </div>
+                <div class="w-1/3 flex space-x-2 justify-center items-center hover:bg-gray-100 dark:hover:bg-dark-third text-xl py-2 rounded-lg cursor-pointer text-gray-500 dark:text-dark-txt">
+                    <i class='bx bx-share bx-flip-horizontal'></i>
+                    <span class="text-sm font-semibold">Share</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END POST ACTION -->
+
+    <!-- LIST COMMENT -->
+    <div class="py-2 px-4">
+        <!-- COMMENT -->
+        <div class="flex space-x-2">
+            <img src="./images/avt-5.jpg" alt="Profile picture" class="w-9 h-9 rounded-full">
+            <div>
+                <div class="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                    <span class="font-semibold block">John Doe</span>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                </div>
+                <div class="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                    <span class="font-semibold cursor-pointer">Like</span>
+                    <span>.</span>
+                    <span class="font-semibold cursor-pointer">Reply</span>
+                    <span>.</span>
+                    10m
+                </div>
+                <!-- COMMENT -->
+                <div class="flex space-x-2">
+                    <img src="./images/avt-7.jpg" alt="Profile picture" class="w-9 h-9 rounded-full">
+                    <div>
+                        <div class="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                            <span class="font-semibold block">John Doe</span>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                        </div>
+                        <div class="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                            <span class="font-semibold cursor-pointer">Like</span>
+                            <span>.</span>
+                            <span class="font-semibold cursor-pointer">Reply</span>
+                            <span>.</span>
+                            10m
+                        </div>
+                    </div>
+                </div>
+                <!-- END COMMENT -->
+            </div>
+        </div>
+        <!-- END COMMENT -->
+        <!-- COMMENT -->
+        <div class="flex space-x-2">
+            <img src="./images/avt-5.jpg" alt="Profile picture" class="w-9 h-9 rounded-full">
+            <div>
+                <div class="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                    <span class="font-semibold block">John Doe</span>
+                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In voluptate ipsa animi corrupti unde, voluptatibus expedita suscipit, itaque, laudantium accusantium aspernatur officia repellendus nihil mollitia soluta distinctio praesentium nulla eos?</span>
+                </div>
+                <div class="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                    <span class="font-semibold cursor-pointer">Like</span>
+                    <span>.</span>
+                    <span class="font-semibold cursor-pointer">Reply</span>
+                    <span>.</span>
+                    10m
+                </div>
+                <!-- COMMENT -->
+                <div class="flex space-x-2">
+                    <img src="./images/avt-7.jpg" alt="Profile picture" class="w-9 h-9 rounded-full">
+                    <div>
+                        <div class="bg-gray-100 dark:bg-dark-third p-2 rounded-2xl text-sm">
+                            <span class="font-semibold block">John Doe</span>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                        </div>
+                        <div class="p-2 text-xs text-gray-500 dark:text-dark-txt">
+                            <span class="font-semibold cursor-pointer">Like</span>
+                            <span>.</span>
+                            <span class="font-semibold cursor-pointer">Reply</span>
+                            <span>.</span>
+                            10m
+                        </div>
+                    </div>
+                </div>
+                <!-- END COMMENT -->
+            </div>
+        </div>
+        <!-- END COMMENT -->
+    </div>
+    <!-- END LIST COMMENT -->
+
+    <!-- COMMENT FORM -->
+    <div class="py-2 px-4">
+        <div class="flex space-x-2">
+            <img src="./images/tuat.jpg" alt="Profile picture" class="w-9 h-9 rounded-full">
+            <div class="flex-1 flex bg-gray-100 dark:bg-dark-third rounded-full items-center justify-between px-3">
+                <input type="text" placeholder="Write a comment..." class="outline-none bg-transparent flex-1">
+                <div class="flex space-x-0 items-center justify-center">
+                    <span class="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl"><i class='bx bx-smile'></i></span>
+                    <span class="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl"><i class='bx bx-camera'></i></span>
+                    <span class="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl"><i class='bx bxs-file-gif'></i></span>
+                    <span class="w-7 h-7 grid place-items-center rounded-full hover:bg-gray-200 cursor-pointer text-gray-500 dark:text-dark-txt dark:hover:bg-dark-second text-xl"><i class='bx bx-happy-heart-eyes'></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END COMMENT FORM -->
+</div>
+<!-- END POST -->
+
 </div>
 
-<style>
-.forum-image {
-  object-fit: cover;
-  object-position: center;
-}
-
-.comment-image {
-  max-width: 100%;
-  height: auto;
-  border-radius: 0.375rem;
-  filter: none; /* Remove blur effect */
-}
-
-.font-sans {
-  font-family: sans-serif; /* Replace with your desired font */
-}
-
-</style>
-        
+<!-- END LIST POST -->
        `;
        
         })
