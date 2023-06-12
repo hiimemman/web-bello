b9ae10fb48677782739aade2ecf980211317f7db
joegrayson <kingjoelunas@gmail.com>
2023-06-12 19:04:22 +0800

    Some changes on view-announcements.php

--- web-bello/pages/view-announcements.php
+++ web-bello/pages/view-announcements.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once('../components/navbar.php')
 ?>
 
@@ -9,30 +9,30 @@ require_once('../components/navbar.php')
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Welcome to Web-Bello Online!</title>
+    <title>Announcements - Web-Bello Online!</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
     <style>
-    .grid-box {
-        background-color: #ffffff;
-        transition: background-color 0.3s;
-        border: solid 2 gray;
-    }
+        .grid-box {
+            background-color: #ffffff;
+            transition: background-color 0.3s;
+            border: solid 2 gray;
+        }
 
-    .grid-box:hover {
-        background-color: #1b263b;
-    }
+        .grid-box:hover {
+            background-color: #1b263b;
+        }
 
-    .grid-box:hover svg {
-        stroke: #fff;
-    }
+        .grid-box:hover svg {
+            stroke: #fff;
+        }
 
-    .grid-box:hover h2 {
-        color: #fff;
-    }
+        .grid-box:hover h2 {
+            color: #fff;
+        }
     </style>
 
     <header class=" border-b-4 border-sky-300">
@@ -57,20 +57,16 @@ require_once('../components/navbar.php')
 
     <section class="border-t-4 border-sky-300">
         <div class="container mx-auto grid gap-8 lg:grid-cols-1 p-6 mt-10">
-            <article
-                class="p-12 mx-20 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <article class="p-12 mx-20 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex justify-between items-center mb-5 text-gray-500">
-                    <span
-                        class="bg-purple-100 text-purple-700 text-md font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                    <span class="bg-purple-100 text-purple-700 text-md font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                         Announcement
                     </span>
                     <span class="text-sm">2023-06-09 01:36:10</span>
                 </div>
                 <div class="flex justify-between items-center">
                     <div class="flex items-center space-x-4">
-                        <img class="w-7 h-7 rounded-full"
-                            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
-                            alt="Bonnie Green avatar" />
+                        <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
                         <span class="font-medium dark:text-white"> John Doe </span>
                     </div>
                 </div>
@@ -90,13 +86,11 @@ require_once('../components/navbar.php')
                     nostrum molestias at aut. Minima quasi saepe maxime quas quibusdam
                     ipsa!
                 </p>
-                <a id="readMoreLink" href="#"
-                    class="mb-5 inline-flex items-center font-medium text-lg text-primary-600 dark:text-primary-500 hover:underline">
+                <a id="readMoreLink" href="#" class="mb-5 inline-flex items-center font-medium text-lg text-primary-600 dark:text-primary-500 hover:underline">
                     See more...
                 </a>
                 <div class="flex justify-center">
-                    <img alt="image" src="https://via.placeholder.com/1920x1080"
-                        class="mt-8 h-auto w-auto object-cover" />
+                    <img alt="image" src="https://via.placeholder.com/1920x1080" class="mt-8 h-auto w-auto object-cover" />
                 </div>
             </article>
         </div>
@@ -159,49 +153,49 @@ require_once('../components/navbar.php')
     <!-- End of Footer-->
 
     <script>
-    const menuToggle = document.getElementById("menu-toggle");
-    menuToggle.addEventListener("click", () => {
-        document.getElementById("menu").classList.toggle("hidden");
-    });
+        const menuToggle = document.getElementById("menu-toggle");
+        menuToggle.addEventListener("click", () => {
+            document.getElementById("menu").classList.toggle("hidden");
+        });
 
-    const closeMenu = document.getElementById("close-menu");
-    closeMenu.addEventListener("click", () => {
-        document.getElementById("menu").classList.toggle("hidden");
-    });
+        const closeMenu = document.getElementById("close-menu");
+        closeMenu.addEventListener("click", () => {
+            document.getElementById("menu").classList.toggle("hidden");
+        });
     </script>
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 
     <script>
-    // Place the JavaScript code here
-    const announcementText = document.getElementById("announcementText");
-    const readMoreLink = document.getElementById("readMoreLink");
+        // Place the JavaScript code here
+        const announcementText = document.getElementById("announcementText");
+        const readMoreLink = document.getElementById("readMoreLink");
 
-    // Check if the text exceeds the line clamp height
-    const checkTextLength = () => {
-        const lineHeight = parseFloat(
-            getComputedStyle(announcementText).lineHeight
-        );
-        const maxHeight = lineHeight * 2; // Maximum height for line clamp of 3
-        const actualHeight = announcementText.scrollHeight;
+        // Check if the text exceeds the line clamp height
+        const checkTextLength = () => {
+            const lineHeight = parseFloat(
+                getComputedStyle(announcementText).lineHeight
+            );
+            const maxHeight = lineHeight * 2; // Maximum height for line clamp of 3
+            const actualHeight = announcementText.scrollHeight;
 
-        if (actualHeight > maxHeight) {
-            readMoreLink.style.display = "inline-flex";
-        } else {
-            readMoreLink.style.display = "none";
-        }
-    };
+            if (actualHeight > maxHeight) {
+                readMoreLink.style.display = "inline-flex";
+            } else {
+                readMoreLink.style.display = "none";
+            }
+        };
 
-    // Toggle line clamp and check text length on load
-    window.addEventListener("load", () => {
-        checkTextLength();
-    });
+        // Toggle line clamp and check text length on load
+        window.addEventListener("load", () => {
+            checkTextLength();
+        });
 
-    // Toggle line clamp and check text length on click
-    readMoreLink.addEventListener("click", (event) => {
-        event.preventDefault(); // Prevent default behavior of the <a> tag
-        announcementText.classList.toggle("line-clamp-2");
-        checkTextLength();
-    });
+        // Toggle line clamp and check text length on click
+        readMoreLink.addEventListener("click", (event) => {
+            event.preventDefault(); // Prevent default behavior of the <a> tag
+            announcementText.classList.toggle("line-clamp-2");
+            checkTextLength();
+        });
     </script>
 </body>
 
