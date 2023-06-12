ac353e14a3d8b52eefeea283fcc72c2e94e553dd
Olarvs <mauuolarvs@gmail.com>
2023-06-12 20:45:27 +0800

    Updating javascript

--- web-bello/calendar-19/get-events.php
+++ web-bello/calendar-19/get-events.php
@@ -5,37 +5,39 @@
   <title>Schedule Event</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script>
-    $(document).ready(function() {
+   document.addEventListener("DOMContentLoaded", function() {
       // Handle form submission
-      $("#schedule-form").submit(function(e) {
+      document.getElementById("schedule-form").addEventListener("submit", function(e) {
         e.preventDefault();
 
         // Retrieve form data
-        var title = $("#title").val();
-        var start_date = $("#start_date").val();
-        var end_date = $("#end_date").val();
+        let title = document.getElementById("title").value;
+        let start_date = document.getElementById("start_date").value;
+        let end_date = document.getElementById("end_date").value;
 
         // Send the data to the server
-        $.ajax({
-          type: "POST",
-          url: "../api/schedule/sched.php",
-          data: { title: title, start_date: start_date, end_date: end_date },
-          dataType: "json",
-          success: function(response) {
-            if (response.status === "success") {
-              alert("Event scheduled successfully.");
-              // Clear the form
-              $("#title").val("");
-              $("#start_date").val("");
-              $("#end_date").val("");
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "../api/schedule/sched.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              let response = JSON.parse(xhr.responseText);
+              if (response.status === "success") {
+                alert("Event scheduled successfully.");
+                // Clear the form
+                document.getElementById("title").value = "";
+                document.getElementById("start_date").value = "";
+                document.getElementById("end_date").value = "";
+              } else {
+                alert("Error: " + response.message);
+              }
             } else {
-              alert("Error: " + response.message);
+              alert("An error occurred: " + xhr.status);
             }
-          },
-          error: function(xhr, status, error) {
-            alert("An error occurred: " + error);
           }
-        });
+        };
+        xhr.send("title=" + encodeURIComponent(title) + "&start_date=" + encodeURIComponent(start_date) + "&end_date=" + encodeURIComponent(end_date));
       });
     });
   </script>
