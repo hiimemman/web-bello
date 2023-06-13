4917e9e842dff03afac7667dceee3cab359b9225
Jeric Valderama <102523400+Olarvs@users.noreply.github.com>
2023-06-14 07:39:04 +0800

    Update view-announcements.php

--- web-bello/pages/view-announcements.php
+++ web-bello/pages/view-announcements.php
@@ -295,10 +295,9 @@ require_once('../components/navbar.php')
                 <h2 class="mt-6 text-2xl font-bold tracking-tight text-gray-900">
                 ` + forum
                 .title + `
-                
-    
-    ` + forum.message_body + `
-    </div>
+                </div>
+                <p>` + forum.message_body + `</p>
+
                 <!-- Image -->
                 <div class="flex justify-center">
                     <img alt="image" src="https://via.placeholder.com/1920x1080"
