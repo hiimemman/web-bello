877051cfc396d7a27baccecd01d232b646e20fd7
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-13 22:54:00 +0800

    test

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -261,7 +261,7 @@ th.sort-desc::after {
                 <div class="grid gap-4 mb-4 sm:grid-cols-2">
                     <input type ="hidden" name ="idUpdate" id ="idUpdate" >
                     <div>
-                        <label for="balanceUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+                        <label for="balanceUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Balance</label>
                         <input type="text" name="balanceUpdate" id="balanceUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                     </div>
 
