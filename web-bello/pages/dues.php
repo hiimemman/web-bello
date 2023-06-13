01e11be350e61b7b480da0e2ecea1309c0af4517
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-14 00:45:24 +0800

    remove payment id

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -250,7 +250,7 @@ th.sort-desc::after {
 
             </div>
             
-            <input type ="hidden" name ="idUpdate" id ="idUpdate" >
+          
                     <div>
                         <label for="amountUpdateFix" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
                         <input type="text" disabled name="amountUpdateFix" id="amountUpdateFix"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
