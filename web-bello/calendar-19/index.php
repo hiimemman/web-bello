cbea1278556a5469fc1d92bb3c8012d28facf76f
joegrayson <kingjoelunas@gmail.com>
2023-06-14 05:14:34 +0800

    test changes

--- web-bello/calendar-19/index.php
+++ web-bello/calendar-19/index.php
@@ -26,8 +26,8 @@ require_once('../components/navbar.php')
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet"> -->
     <link rel="stylesheet" href="fonts/icomoon/style.css">
     <link href='fullcalendar/packages/core/main.css' rel='stylesheet' />
     <link href='fullcalendar/packages/daygrid/main.css' rel='stylesheet' />
