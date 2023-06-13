7c0f6f868e6b5fe09db0ad71b6facc88d31ed8ee
joegrayson <kingjoelunas@gmail.com>
2023-06-14 04:18:58 +0800

    Changes on hrefs on footer in user-index

--- web-bello/pages/user-index.php
+++ web-bello/pages/user-index.php
@@ -212,19 +212,22 @@ require_once('../components/navbar.php')
 
                 <ul class="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12">
                     <li>
-                        <a class="text-gray-700 font-medium transition hover:text-gray-700/75" href="/">
+                        <a class="text-gray-700 font-medium transition hover:text-gray-700/75"
+                            href="/web-bello/pages/view-announcements.php">
                             Announcements
                         </a>
                     </li>
 
                     <li>
-                        <a class="text-gray-700 font-medium transition hover:text-gray-700/75" href="/">
+                        <a class="text-gray-700 font-medium transition hover:text-gray-700/75"
+                            href="/web-bello/pages/view-events.php">
                             Events
                         </a>
                     </li>
 
                     <li>
-                        <a class="text-gray-700 font-medium transition hover:text-gray-700/75" href="/">
+                        <a class="text-gray-700 font-medium transition hover:text-gray-700/75"
+                            href="/web-bello/pages/view-forums.php">
                             Forums
                         </a>
                     </li>
