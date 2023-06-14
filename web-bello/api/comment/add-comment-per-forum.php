dd5f5a07ea3c9a627363b93f20f7478c9bf89603
Jeric Valderama <102523400+Olarvs@users.noreply.github.com>
2023-06-14 08:41:25 +0800

    Update add-comment-per-forum.php

--- web-bello/api/comment/add-comment-per-forum.php
+++ web-bello/api/comment/add-comment-per-forum.php
@@ -5,34 +5,24 @@ header('Content-type: application/json');
 include_once("../../connections/connection.php");
 $con = connection();
 
-
 $CommentText = $_POST['comment_text'];
 $ForumId = $_POST['forum_id'];
 $UserFullName = $_POST['user_full_name'];
 $UserId = $_POST['user_id'];
 $UserEmail = $_POST['user_email'];
 $ImageUrl = $_POST['image_url'];
-try{
-    
 
-
-    $sql = "INSERT INTO `tbl_comments` (`comment_text`,`forum_id`,`user_full_name`,`user_id`,`user_email`,`image_url`) VALUES ('$CommentText','$ForumId','$UserFullName', '$UserId','$UserEmail','$ImageUrl');";
+try {
+    $sql = "INSERT INTO `tbl_comments` (`comment_text`,`forum_id`,`user_full_name`,`user_id`,`user_email`,`image_url`) VALUES ('$CommentText','$ForumId','$UserFullName', '$UserId','$UserEmail','$ImageUrl')";
     mysqli_query($con, $sql);
 
-    //get all if success
+    // Retrieve all comments after successful insertion
     $sqlGet = mysqli_query($con, "SELECT * FROM `tbl_comments` ORDER BY `created_at` DESC");
-
-    //store in result
-
     $result = mysqli_fetch_all($sqlGet, MYSQLI_ASSOC);
 
-// Redirect to the specified URL
-header("Location: https://web-bello.online/web-bello/pages/view-announcements.php");
-exit();
-  
-}catch(Exception $e){
-    exit(json_encode(array("responseStatus" =>'error', "responseContent" =>$e->getMessage(), "responseMessage" =>'Comment new home owner failed error:!')));
+    // Return the response as JSON
+    echo json_encode(array("responseStatus" => 'success', "responseContent" => $result, "responseMessage" => 'Comment added successfully!'));
+} catch (Exception $e) {
+    echo json_encode(array("responseStatus" => 'error', "responseContent" => $e->getMessage(), "responseMessage" => 'Failed to add comment!'));
 }
-
-
 ?>
