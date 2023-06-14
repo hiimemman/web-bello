6950ba6d2c63c0e658ec44deb725d110a225498f
emannocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-14 12:17:57 +0800

    test

--- web-bello/pages/monthly-due.php
+++ web-bello/pages/monthly-due.php
@@ -289,7 +289,7 @@ const loadTable = async function() {
   if (response.responseStatus === 'success') {
     let content = '';
 
-    JSON.parse(response.responseContent).forEach((contents) => {
+    response.responseContent.forEach((contents) => {
       let contentStatus = '<span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">Unpaid</span>';
 
       if (contents.status === 'paid') {
