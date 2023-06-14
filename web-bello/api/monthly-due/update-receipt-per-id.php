2ff5c115e1ccfa23d6c534ef2763db5681fd0287
emannocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-14 13:43:35 +0800

    test

---
+++ web-bello/api/monthly-due/update-receipt-per-id.php
@@ -0,0 +1,27 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+ 
+ include_once("../../connections/connection.php");
+ $con = connection();
+
+$ID = $_POST['idUpdate'];
+$ReceiptURl = $_POST['receiptInput'];
+
+
+try{
+    
+
+
+    $sql = "UPDATE `tbl_payment` SET `receipt_url` ='$ReceiptURl'WHERE `tbl_payment`.`payment_id` = '$ID';";
+    mysqli_query($con, $sql);
+
+   
+    
+    exit(json_encode(array("responseStatus" =>'success', "responseContent" =>'reload', "responseMessage" =>'Monthly due has been updated!')));
+
+}catch(Exception $e){
+    exit(json_encode(array("responseStatus" =>'error', "responseContent" =>$e->getMessage(), "responseMessage" =>'Update failed error:!')));
+}
+
+?>
--- web-bello/pages/monthly-due.php
+++ web-bello/pages/monthly-due.php
@@ -190,7 +190,7 @@ require_once('../components/navbar.php');
                         <div id ="imageviewer" >
                             
                         </div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="receiptInput">Upload file</label>
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="receiptInput">Upload a receipt</label>
                     <input id ="receiptInput" name ="receiptInput" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" type="file">
                     <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
 
@@ -397,7 +397,7 @@ updateHoaInformation.addEventListener('submit', async (event) =>{
 }
 
 //fetch data
-const request =  await fetch("../api/monthly-due/all-monthly-due-update.php",{
+const request =  await fetch("../api/monthly-due/update-receipt-per-id.php",{
     method: "POST",
     body:formData,
   });
