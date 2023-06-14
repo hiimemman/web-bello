51172bea7d84f86914f68f4656868dd6c29c067f
emannocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-14 11:50:57 +0800

    test

--- web-bello/pages/monthly-due.php
+++ web-bello/pages/monthly-due.php
@@ -207,7 +207,10 @@ require_once('../components/navbar.php');
     </footer>
     
 
-
+<!-- HIDDEN BUTTON -->
+<button data-modal-target="updateProductModal" data-modal-toggle="updateProductModal" class="hidden block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" id ="updateHiddenButton">
+  Toggle modal
+</button>
 
 
 <script defer src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
