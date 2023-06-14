bb535ee65a5a404d59073b6b15b9578c2fd721d0
Jeric Valderama <102523400+Olarvs@users.noreply.github.com>
2023-06-14 10:37:15 +0800

    Update schedule.php

--- web-bello/calendar-19/schedule.php
+++ web-bello/calendar-19/schedule.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+include_once("../connections/connection.php");
+$con = connection();
+if (!isset($_SESSION['ID'])) {
+  header('Location: ../pages/user-login.php');
+  exit();
+} else {
+  $sql = mysqli_query($con, "SELECT * FROM `tbl_residents` WHERE `id` = {$_SESSION['ID']}");
+  $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+  $url = $_SERVER['REQUEST_URI'];
+  $pageName = basename($url, '.php');
+  $pageName = str_replace('-', ' ', $pageName);
+  $pageName = strtoupper($pageName);
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -15,92 +32,87 @@
     <!-- Style -->
     <link rel="stylesheet" href="css/style.css">
 
-    <style>
-        body {
-            background-color: #1a202c;
-        }
-
-        .content {
-            color: #fff;
-        }
-
-        footer {
-            background-color: #2d3748;
-            color: #fff;
-        }
-
-        footer a,
-        footer a:hover {
-            color: #fff;
-        }
-    </style>
-
     <title>Community Schedules - Web-Bello Online!</title>
+
+    <style>
+    /* Modal Styles */
+    .modal-container {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 400px;
+      height: 300px;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    }
+
+    .modal-container h1 {
+      margin-top: 0;
+    }
+
+    .modal-container label {
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    .modal-container input[type="text"],
+    .modal-container input[type="datetime-local"],
+    .modal-container input[type="submit"] {
+      width: 100%;
+      padding: 5px;
+      margin-bottom: 10px;
+    }
+
+    .modal-container input[type="submit"] {
+      background-color: #4caf50;
+      color: #fff;
+      border: none;
+      cursor: pointer;
+    }
+
+    .modal-close {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+    }
+  </style>
+
 
 </head>
 
 <body>
     <div class="content">
+    <button id="addScheduleBtn" class="btn btn-primary">Add Schedule</button>
         <div id='calendar'></div>
     </div>
 
-    <!-- Add Event Modal -->
-    <div id="addEventModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="addEventModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addEventModalLabel">Add Event</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <!-- Add event form goes here -->
-                    <form>
-                        <div class="form-group">
-                            <label for="eventName">Event Name</label>
-                            <input type="text" class="form-control" id="eventName" placeholder="Enter event name">
-                        </div>
-                        <div class="form-group">
-                            <label for="eventDate">Event Date</label>
-                            <input type="date" class="form-control" id="eventDate">
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="addEventBtn">Add Event</button>
-                </div>
-            </div>
+     <!-- Modal -->
+     <div id="addScheduleModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <form id="scheduleForm">
+                <h1>Schedule an Event</h1>
+                <form id="schedule-form">
+                <label for="title">Title:</label>
+                <input type="text" name="title" id="title" required>
+                <br><br>
+                <label for="start_date">Start Date:</label>
+                <input type="datetime-local" name="start_date" id="start_date" required>
+                <br><br>
+                <label for="end_date">End Date:</label>
+                <input type="datetime-local" name="end_date" id="end_date" required>
+                <br><br>
+                <input type="submit" value="Schedule Event">
+            </form>
         </div>
     </div>
-    <!-- End of Add Event Modal -->
 
-    <!-- Remove Event Modal -->
-    <div id="removeEventModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="removeEventModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="removeEventModalLabel">Remove Event</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Are you sure you want to remove this event?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-danger" id="removeEventBtn">Remove Event</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- End of Remove Event Modal -->
-
-    <footer class="bg-gray-900">
+    <!-- Footer -->
+    <footer class="bg-gray-100">
         <img src="../" alt="" srcset="">
         <div class="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-24">
             <div class="lg:flex lg:items-end lg:justify-between">
@@ -152,6 +164,8 @@
             </p>
         </div>
     </footer>
+    <!-- End of Footer-->
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
@@ -161,46 +175,98 @@
     <script src='fullcalendar/packages/daygrid/main.js'></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: ['interaction', 'dayGrid'],
-                defaultDate: '2023-06-12',
-                editable: true,
-                eventLimit: true, // allow "more" link when too many events,
-                events: {
-                    url: '../api/schedule/all-sched.php',
-                    method: 'POST',
-                    extraParams: {
-                        user_id: <?php echo $_SESSION['ID']; ?> // Pass the user ID to the server
-                    },
-                    failure: function (xhr, status, error) {
-                        console.log(xhr.responseText); // Print the error response
-                        alert('Failed to fetch events from the server.' + failure);
-                    }
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            plugins: ['interaction', 'dayGrid'],
+            defaultDate: '2023-06-12',
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events,
+            events: {
+                url: '../api/schedule/all-sched.php',
+                method: 'POST',
+                extraParams: {
+                    user_id: <?php echo $_SESSION['ID']; ?> // Pass the user ID to the server
+                },
+                failure: function(xhr, status, error) {
+                    console.log(xhr.responseText); // Print the error response
+                    alert('Failed to fetch events from the server.' + failure);
                 }
-            });
-            calendar.render();
-
-            // Add Event Button Click Handler
-            $('#addEventBtn').on('click', function () {
-                var eventName = $('#eventName').val();
-                var eventDate = $('#eventDate').val();
-                // Add your logic to handle the new event here
-                // You can use the eventName and eventDate variables to get the values from the form
-                // Example: calendar.addEvent({ title: eventName, start: eventDate });
-                // After adding the event, close the modal
-                $('#addEventModal').modal('hide');
-            });
-
-            // Remove Event Button Click Handler
-            $('#removeEventBtn').on('click', function () {
-                // Add your logic to remove the selected event here
-                // Example: calendar.getEventById(eventId).remove();
-                // After removing the event, close the modal
-                $('#removeEventModal').modal('hide');
-            });
+            }
         });
+        calendar.render();
+    });
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+            var modal = document.getElementById("addScheduleModal");
+            var addScheduleBtn = document.getElementById("addScheduleBtn");
+            var closeBtn = document.getElementsByClassName("close")[0];
+
+            // Open the modal when the button is clicked
+            addScheduleBtn.onclick = function () {
+                modal.style.display = "block";
+            }
+
+            // Close the modal when the close button is clicked
+            closeBtn.onclick = function () {
+                modal.style.display = "none";
+            }
+
+            // Close the modal when clicked outside of it
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        });
+
+document.addEventListener("DOMContentLoaded", function() {
+      // Handle form submission
+      document.getElementById("schedule-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        // Retrieve form data
+        let title = document.getElementById("title").value;
+        let start_date = document.getElementById("start_date").value;
+        let end_date = document.getElementById("end_date").value;
+
+        // Send the data to the server
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "../api/schedule/sched.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              let response = JSON.parse(xhr.responseText);
+              if (response.status === "success") {
+                alert("Event scheduled successfully.");
+                // Clear the form
+                document.getElementById("title").value = "";
+                document.getElementById("start_date").value = "";
+                document.getElementById("end_date").value = "";
+              } else {
+                alert("Error: " + response.message);
+              }
+            } else {
+              alert("An error occurred: " + xhr.status);
+            }
+          }
+        };
+        xhr.send("title=" + encodeURIComponent(title) + "&start_date=" + encodeURIComponent(start_date) + "&end_date=" + encodeURIComponent(end_date));
+      });
+
+      // Close the modal when the close button is clicked
+      document.getElementById("modal-close").addEventListener("click", function() {
+        document.getElementById("modal-container").style.display = "none";
+      });
+    });
+
+function openModal() {
+      document.getElementById("modal-container").style.display = "block";
+    }
+
     </script>
 </body>
 
