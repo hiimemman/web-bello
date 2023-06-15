e240a80daed668d63820155a5d447d97a4e2a395
joegrayson <kingjoelunas@gmail.com>
2023-06-15 10:21:00 +0800

    Test 2

--- web-bello/pages/profile.php
+++ web-bello/pages/profile.php
@@ -43,6 +43,9 @@ require_once('../components/navbar.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web-Bello | Profile</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <style>
         body {
             font-family: Arial, sans-serif;
