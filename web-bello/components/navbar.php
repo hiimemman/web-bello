5c58fa1d06e74f82d6c0ec2fa47070830a18eb77
Olarvs <mauuolarvs@gmail.com>
2023-06-19 19:18:37 +0800

    nav

--- web-bello/components/navbar.php
+++ web-bello/components/navbar.php
@@ -93,6 +93,16 @@ if (!isset($_SESSION['IDUSER'])) {
                     </svg>
                 </button>
             </div>
+            <div class="-ml-2 mr-2 flex items-center md:hidden">
+                        <!-- Mobile menu button -->
+                        <button type="button" class="bg-sky-500 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+                            <span class="sr-only">Open main menu</span>
+                            <!-- Heroicon name: outline/menu -->
+                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </button>
+                    </div>
 
             <!-- Nav Links -->
             <div class="hidden lg:flex lg:gap-x-12" id="navbar-default">
