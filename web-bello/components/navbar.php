aad93071b030ed1232183334e43a161e5b3ecd39
Olarvs <mauuolarvs@gmail.com>
2023-06-19 19:15:17 +0800

    nav

--- web-bello/components/navbar.php
+++ web-bello/components/navbar.php
@@ -129,7 +129,7 @@ if (!isset($_SESSION['IDUSER'])) {
     </nav>
 
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2" defer></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com" defer></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     <script defer>
