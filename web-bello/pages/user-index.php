4fbb847b0fa35da860bc95923f42138b75e21561
Olarvs <mauuolarvs@gmail.com>
2023-06-20 00:27:53 +0800

    sending mailto

--- web-bello/pages/user-index.php
+++ web-bello/pages/user-index.php
@@ -10,7 +10,8 @@ require_once('../components/navbar.php')
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Welcome to Web-Bello Online!</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com/2.2.19/tailwind.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
