9dfd617adb4df25943c7063622ecf9c807c48b56
Olarvs <mauuolarvs@gmail.com>
2023-06-20 00:20:24 +0800

    modal on backend of contact form

--- web-bello/api/mailto/mailto.php
+++ web-bello/api/mailto/mailto.php
@@ -4,18 +4,73 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $message = $_POST['message'];
     
-    $to = 'web-bello@gmail.com';
+    $to = 'manuelolarve@gmail.com';
     $subject = 'New Contact Form Submission';
     $body = "Name: $name\nEmail: $email\nMessage: $message";
     $headers = "From: $email";
     
     // Send email
-    if (mail($to, $subject, $body, $headers)) {
-        // Email sent successfully
-        echo "Thank you for your message!";
-    } else {
-        // Error sending email
-        echo "Sorry, there was an error sending your message. Please try again later.";
-    }
+  // Send email
+  if (mail($to, $subject, $body, $headers)) {
+    // Email sent successfully
+    echo '
+    <script>
+        setTimeout(function() {
+            document.getElementById("successModal").classList.add("hidden");
+        }, 1000);
+    </script>
+    <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline" id="successModal">
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div class="sm:flex sm:items-start">
+                        <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <svg class="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
+                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">Thank you for your message!</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+} else {
+    // Error sending email
+    echo '
+    <script>
+        setTimeout(function() {
+            document.getElementById("errorModal").classList.add("hidden");
+        }, 1000);
+    </script>
+    <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline" id="errorModal">
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div class="sm:flex sm:items-start">
+                        <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                            </svg>
+                        </div>
+                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">Sorry, there was an error sending your message. Please try again later.</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+}
 }
 ?>
