2336c9d7fabebf1523bf49343f295d90b8f7e366
Olarvs <mauuolarvs@gmail.com>
2023-06-20 20:25:04 +0800

    changing default page

--- index.php
+++ index.php
@@ -14,11 +14,6 @@ if (isset($_SESSION['IDUSER'])) {
     <title>Home - Welcome to Web-Bello</title>
     <link rel="stylesheet" href="../style.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-</head>
-
-
-
-<body>
     <style>
         .custom-bg {
             background-image: url('/web-bello/images/palazzo-gradient-1.png');
@@ -61,7 +56,6 @@ if (isset($_SESSION['IDUSER'])) {
             color: #023e8a; 
         }
 
-        
         @keyframes fade-in {
             0% {
                 opacity: 0;
@@ -74,55 +68,50 @@ if (isset($_SESSION['IDUSER'])) {
         .landing-content {
             animation: fade-in 2s ease-in-out;
         }
+
+        .container {
+            max-width: 100%;
+            padding: 0 20px;
+            margin: 0 auto;
+        }
     </style>
-    
-    <!-- LANDING PAGE SECTION PLUS LOGIN FORM -->
-
-    <section class="bg-white dark:bg-gray-900 custom-bg">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h1
-                    class="landing-content max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-                    Welcome to Web-Bello!</h1>
-                <p class="landing-content max-w-2xl mb-6 font-medium text-white lg:mb-8 md:text-lg lg:text-xl dark:text-white">
-                    The
-                    ultimate hub for homeowners to stay connected and informed! With Web-Bello, you can easily connect
-                    with your neighbors, stay informed on the latest news and events, and receive important alerts and
-                    notifications.</p>
-                    
-                    <!--<button style="color: white; class="p-4 rounded-lg landing-content">About Us</button>-->
-            </div>
-            <div class="landing-content lg:mt-0 lg:col-span-5 lg:flex">
-                <div
-                    class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1
-                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
-                        </h1>
-                        
-                        <div class="space-y-4 md:space-y-6">
-                            <a href="/web-bello/pages/user-login.php">
-                                <button type="submit" class="custom-signin-btn">
-                                    Click here
-                                </button>
-                            </a>
-
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Or <a href="/web-bello/pages/index.php"
-                                    class="custom-text-btn">sign in
-                                    here</a> as administrator.
-                            </p>
+</head>
+<body>
+    <div class="container">
+        <section class="bg-white dark:bg-gray-900 custom-bg">
+            <div class="grid max-w-screen-xl px-4 py-8 mx-auto grid-cols-1 md:grid-cols-12">
+                <div class="mr-auto place-self-center md:col-span-7">
+                    <h1 class="landing-content max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
+                        Welcome to Web-Bello!
+                    </h1>
+                    <p class="landing-content max-w-2xl mb-6 font-medium text-white md:mb-8 md:text-lg lg:text-xl dark:text-white">
+                        The ultimate hub for homeowners to stay connected and informed! With Web-Bello, you can easily connect
+                        with your neighbors, stay informed on the latest news and events, and receive important alerts and
+                        notifications.
+                    </p>
+                </div>
+                <div class="landing-content md:mt-0 md:col-span-5 md:flex">
+                    <div class="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Sign in to your account
+                            </h1>
+                            <div class="space-y-4 md:space-y-6">
+                                <a href="/web-bello/pages/user-login.php">
+                                    <button type="submit" class="custom-signin-btn">
+                                        Click here
+                                    </button>
+                                </a>
+                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Or <a href="/web-bello/pages/index.php" class="custom-text-btn">sign in here</a> as administrator.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-
-    <!-- END OF LANDING PAGE SECTION PLUS LOGIN FORM -->
-    
-
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+        </section>
+    </div>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
 </html>
