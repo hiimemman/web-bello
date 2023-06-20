6e4e1da06988b3d1d950b5a36c62efd615c3dc67
joegrayson <kingjoelunas@gmail.com>
2023-06-21 01:57:35 +0800

    NavBar Responsiveness Test 13

--- web-bello/components/navbar.php
+++ web-bello/components/navbar.php
@@ -203,7 +203,7 @@ if (!isset($_SESSION['IDUSER'])) {
     <div class="hidden" role="dialog" aria-modal="true" id="menu">
         <div class="fixed inset-0 z-10">
             <div
-                class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-10 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-gray-900 px-6 py-10 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
                     <a href="#" class="-m-1.5 p-1.5">
                         <span class="sr-only">Web-Bello</span>
