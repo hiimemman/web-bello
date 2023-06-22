f32043920021b3fa24f7b884b0ac4bd56eb90e00
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-22 18:22:56 +0800

    Test bg

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -45,11 +45,11 @@ th.sort-desc::after {
 
 
 <body class ="-ml-96">
-    <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 flex h-screen w-full">
+  <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 flex h-screen w-full">
     <div class="mx-auto w-full px-4 lg:px-12">
         <!-- Start coding here -->
-        <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+        <div class="bg-white dark:bg-gray-800 bg-xl relative shadow-md sm:rounded-lg overflow-hidden">
+       <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full md:w-1/2">
                     <form class="flex items-center">
                         <label for="simple-search" class="sr-only">Search</label>
