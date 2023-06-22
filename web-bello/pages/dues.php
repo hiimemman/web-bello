ada9c2eafeb676374670b8874a329bd354479cd1
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-22 18:25:08 +0800

    Test dues

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -48,7 +48,7 @@ th.sort-desc::after {
   <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 flex h-screen w-full">
     <div class="mx-auto w-full px-4 lg:px-12">
         <!-- Start coding here -->
-        <div class="bg-white dark:bg-gray-800 bg-xl relative shadow-md sm:rounded-lg overflow-hidden">
+        <div class="bg-white dark:bg-gray-800 bg-xl relative shadow-md sm:rounded-lg overflow-hidden" style="height: auto; overflow-y: auto;">
        <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full md:w-1/2">
                     <form class="flex items-center">
