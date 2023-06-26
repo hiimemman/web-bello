37368e91bf79f399c949b7df48a96a89b26d7e34
Olarvs <mauuolarvs@gmail.com>
2023-06-26 19:01:57 +0800

    New announcement

--- web-bello/api/forum/new-announcement.php
+++ web-bello/api/forum/new-announcement.php
@@ -6,7 +6,7 @@ try{
 $con = connection();
 
 
-$sql = mysqli_query($con, "SELECT * FROM `tbl_forum` WHERE `category` = 'Announcement' AND 'Event' ORDER BY `created_at` DESC");
+$sql = mysqli_query($con, "SELECT * FROM `tbl_forum` WHERE `category` = 'Announcement', 'Event' ORDER BY `created_at` DESC");
 
 //store in result
 
