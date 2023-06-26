e0702ce6c2b25969de45e05a5e795d6e7708a161
Olarvs <mauuolarvs@gmail.com>
2023-06-26 18:09:01 +0800

    New forum page

--- web-bello/pages/sample2.php
+++ web-bello/pages/sample2.php
@@ -209,7 +209,6 @@ th.sort-desc::after {
                     <div>
                         <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                             <select id="category" name ="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
-                            <option disabled selected>Category</option>
                             <option value="Forum">Forum</option>
                             
                             </select>
