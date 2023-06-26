b164e3b7e1c4db007077baf032a544167aec2582
Olarvs <mauuolarvs@gmail.com>
2023-06-26 13:38:46 +0800

    dues chart

--- web-bello/pages/try.php
+++ web-bello/pages/try.php
@@ -125,7 +125,21 @@ window.onload = function() {
                 }]
             });
             chart.render();
-        }
+        
+
+var duesChart = new CanvasJS.Chart("duesChartContainer", {
+    animationEnabled: true,
+    theme: "dark2",
+    title: {
+        text: "Payment Status"
+    },
+    data: [{
+        type: "column",
+        dataPoints: <?php echo json_encode($duesDataPoints, JSON_NUMERIC_CHECK); ?>
+    }]
+});
+duesChart.render();
+}
 </script>
 </head>
 
@@ -199,7 +213,7 @@ window.onload = function() {
         
     </div>
     <!-- Chart -->
-    <!-- <div id="chartContainer" style="height: 370px; width: 100%;"></div> -->
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 
     <!-- New Chart for Paid and Unpaid Dues -->
     <div id="duesChartContainer" style="height: 370px; width: 100%;"></div>
