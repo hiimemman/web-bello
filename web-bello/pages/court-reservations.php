f590fa716e5126a2c645b70e6e34445c22beeeb5
Olarvs <mauuolarvs@gmail.com>
2023-06-28 18:27:05 +0800

    New reservation revert

--- web-bello/pages/court-reservations.php
+++ web-bello/pages/court-reservations.php
@@ -65,7 +65,7 @@ th.sort-desc::after {
 
                
                 <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                   <button type="button" class="flex: items-center justify-center focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" >New Resident</button>
+                   <button type="button" class="flex: items-center justify-center focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" >New Official</button>
                     <div class="flex items-center space-x-3 w-full md:w-auto">
                         <!-- <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
                             <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -124,11 +124,13 @@ th.sort-desc::after {
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400" id ="tblHOA">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Title</th>
-                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Name</th>
-                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Start Date-Time</th>
-                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">End Date-Time</th>
+                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Full Name</th>
+                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Email</th>
+                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Role</th>
+                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Address</th>
+                            <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Status</th>
                             <th scope="col" class="px-4 py-3" onClick ="addTableSorting(tblHOA)">Date Created</th>
+                            <th scope="col" class="px-4 py-3">
                                 <span class="sr-only">Actions</span>
                             </th>
                         </tr>
@@ -140,13 +142,13 @@ th.sort-desc::after {
                          
                         
                     </tbody>
+
                 </table>
+    
             </div>
-            
+
         </div>
-
     </div>
-
 
       
 <!-- TOASTER -->
@@ -190,26 +192,34 @@ th.sort-desc::after {
                 <span class="sr-only">Close modal</span>
             </button>
             <div class="px-6 py-6 lg:px-8">
-                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add new reservation</h3>
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add new home owner</h3>
                 <form class="space-y-6" action="#" id ="frmRegisterHOA">
                     <div>
-                        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                        <input type="title" name="firstname" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
+                        <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                        <input type="firstname" name="firstname" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
                     </div>
                     <div>
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
+                        <label for="lastname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                        <input type="lastname" name="lastname" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
                     </div>
                     <div>
-                        <label for="start" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date-Time</label>
-                        <input type="start" name="start" id="start" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
                     </div>
                     <div>
-                        <label for="end" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End Date-Time</label>
-                        <input type="end" name="end" id="end" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
+                        <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                            <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                            <option selected>Choose a role</option>
+                            <option value="Admin">Admin</option>
+                            <option value="Board member">Board member</option>
+                            <option value="Home owner">Home owner</option>
+                            </select>
                     </div>
-
-                    <button type="submit" class="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">Create Reservation</button>
+                    <div>
+                        <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                        <input type="address" name="address" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  required>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">Create Account</button>
                    
                 </form>
             </div>
@@ -227,7 +237,7 @@ th.sort-desc::after {
             <!-- Modal header -->
             <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Update Reservation
+                    Update Hoa
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="updateProductModal">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -252,40 +262,22 @@ th.sort-desc::after {
                     </div>
 
                     <div>
-                        <label for="sexUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sex</label>
-                        <select id="sexUpdate" name ="sexUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                    </div>
-
-                    <div>
-                        <label for="ageUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
-                        <input type="text" name="ageUpdate" id="ageUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                        <label for="emailUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="text" name="emailUpdate" id="emailUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                     </div>
 
                     <div>
                         <label for="addressUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
                         <input type="text" name="addressUpdate" id="addressUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                     </div>
-
-                    <div>
-                        <label for="contactUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">contact</label>
-                        <input type="text" name="contactUpdate" id="contactUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                    </div>
                     
                     <div>
-                        <label for="emailUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="text" name="emailUpdate" id="emailUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                    </div>
-                    <div>
-                        <label for="paymentUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
-                        <select id="paymentUpdate" name ="paymentUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option value="unpaid to this month">unpaid to this month</option>
-                            <option value="payments up to date">payments up to date</option>
+                        <label for="roleUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                        <select id="roleUpdate" name ="roleUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="admin">Admin</option>
+                            <option value="board member">Board member</option>
                         </select>
                     </div>
-                    
                     <div>
                         <label for="statusUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
                         <select id="statusUpdate" name ="statusUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
@@ -367,13 +359,10 @@ const updateHoaInformation = document.querySelector('#updateHoaInformation')
 const idUpdate =document.querySelector('#idUpdate')
 const firstnameUpdate =document.querySelector('#firstnameUpdate')
 const lastnameUpdate =document.querySelector('#lastnameUpdate')
-const sexUpdate =document.querySelector('#sexUpdate')
-const ageUpdate =document.querySelector('#ageUpdate')
-const addressUpdate = document.querySelector('#addressUpdate')
-const contactUpdate = document.querySelector('#contactUpdate')
 const emailUpdate = document.querySelector('#emailUpdate')
+const addressUpdate = document.querySelector('#addressUpdate')
+const roleUpdate =document.querySelector('#roleUpdate')
 const statusUpdate =document.querySelector('#statusUpdate')
-const paymentUpdate =document.querySelector('#paymentUpdate')
 
 
 //Onload
@@ -384,7 +373,7 @@ window.onload = function(){
 //load table
 const loadTable = async function(){
     //api
-  const getUsers =  await fetch("../api/reservation/all-reservation.php");
+  const getUsers =  await fetch("../api/hoa/all-hoa.php");
   const response = await getUsers.json();
     console.log(response)
   if(response.responseStatus === 'OK'){
@@ -400,12 +389,12 @@ const loadTable = async function(){
         }
 
         content += `<tr class="border-b dark:border-gray-700">
-        <td class="px-4 py-3">`+users.title+`</td>
-        <td class="px-4 py-3">`+users.name+`</td>
-        <td class="px-4 py-3">`+users.start_date+`</td>
-        <td class="px-4 py-3">`+users.end_date+`</td>
-        <td class="px-4 py-3">`+users.date_created+`</td>
-        
+        <td class="px-4 py-3">`+users.firstname+" "+users.lastname+`</td>
+        <td class="px-4 py-3">`+users.email+`</td>
+        <td class="px-4 py-3">`+users.role+`</td>
+        <td class="px-4 py-3">`+users.address+`</td>
+        <td class="px-4 py-3">`+user+`</td>
+        <td class="px-4 py-3">`+users.created_at+`</td>
         <td class="px-4 py-3 flex items-center justify-end">
         <div class="inline-flex rounded-md shadow-sm" role="group">
             <button id ="btnView`+users.id+`" type="button" data-modal-toggle = "updateProductModal" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border    border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"  data-user='`+JSON.stringify(users)+`' onclick="updateModal(this)" >
@@ -441,7 +430,7 @@ frmRegisterHOA.addEventListener('submit', async (event) =>{
 }
 
 //fetch data
-  const request =  await fetch("../api/residents/register-new-resident.php",{
+  const request =  await fetch("../api/hoa/register-new-hoa.php",{
     method: "POST",
     body:formData,
   });
@@ -470,7 +459,7 @@ updateHoaInformation.addEventListener('submit', async (event) =>{
 // }
 
 //fetch data
-const request =  await fetch("../api/residents/update-residents.php",{
+const request =  await fetch("../api/hoa/update-hoa.php",{
     method: "POST",
     body:formData,
   });
@@ -546,7 +535,7 @@ const deleteModal = async (button) =>{
 
     formData = new FormData();
     formData.append('IdDelete', user.id)
-    const request =  await fetch("../api/residents/delete-residents.php",{
+    const request =  await fetch("../api/hoa/delete-hoa.php",{
     method: "POST",
     body:formData,
     });
@@ -587,7 +576,7 @@ emailUpdate.value = user.email;
 addressUpdate.value = user.address;
 roleUpdate.value = user.role;
 statusUpdate.value = user.status;
-paymentUpdate.value = user.payment_status;
+
 }
 
 
@@ -612,6 +601,9 @@ const showToast = () => {
 
 // Call showToast when the page loads
 document.addEventListener('DOMContentLoaded', showToast);
+
+
+
 
 
 </script>
