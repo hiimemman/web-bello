bfc64b3c48931670c28c91e276d2557c95574850
Olarvs <mauuolarvs@gmail.com>
2023-06-28 18:37:49 +0800

    Posting reservations

--- web-bello/pages/court-reservations.php
+++ web-bello/pages/court-reservations.php
@@ -383,10 +383,6 @@ const loadTable = async function(){
         //by default inactive
         let user = '<span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">Inactive</span>'
 
-        //change the badge color to active if active
-        if(users.status === 'Active'){
-          user =  '<span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Active</span>'
-        }
 
         content += `<tr class="border-b dark:border-gray-700">
         <td class="px-4 py-3">`+users.name`</td>
