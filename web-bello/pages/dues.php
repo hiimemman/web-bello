654b9b23223ac011090b9f197ef6c3422cc19533
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-28 22:16:02 +0800

    test

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -267,7 +267,9 @@ th.sort-desc::after {
                     
                     <div>
                         <label for="totalPaymentUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total Payment</label>
-                        <input type="text"  name="totalPaymentUpdate" id="totalPaymentUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                        <input type="text" name="totalPaymentUpdate" id="totalPaymentUpdate" pattern="[-+]?[0-9]*\.?[0-9]+" 
+class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+required>
                     </div>
 
                     <div>
@@ -278,7 +280,7 @@ th.sort-desc::after {
                         </select>
                     </div>
                
-                <div class="flex items-center">
+                <div class="flex items-center mt-2">
                     <button type="submit" class="w-full focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">
                         Update 
                     </button>
