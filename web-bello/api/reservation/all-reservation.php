bc54cc59c83cd2c5deac9171e3ae052dc064b75a
Olarvs <mauuolarvs@gmail.com>
2023-06-28 19:28:26 +0800

    Fixing reservation bugs

--- web-bello/api/reservation/all-reservation.php
+++ web-bello/api/reservation/all-reservation.php
@@ -1,20 +1,28 @@
 <?php
-try{
-    header('Access-Control-Allow-Origin: *');
-    header('Content-type: application/json');
-    include_once("../../connections/connection.php");
+include_once("../../connections/connection.php");
 $con = connection();
 
+// Select all events from the database
+$sql = "SELECT * FROM tbl_reservation";
+$result = mysqli_query($con, $sql);
 
-$sql = mysqli_query($con, "SELECT * FROM `tbl_reservation` ORDER BY `date_created` DESC");
+// Check if any events were found
+if (mysqli_num_rows($result) > 0) {
+  $events = array();
 
-//store in result
+  // Iterate through each row and add it to the events array
+  while ($row = mysqli_fetch_assoc($result)) {
+    $event = array(
+      'title' => $row['title'],
+      'start' => $row['start_date'],
+      'end' => $row['end_date']
+    );
 
-$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
-
-exit(json_encode(array("responseStatus" =>'OK', "responseContent" => $result)));
-}catch (Exception $e){
-    exit(json_encode(array("responseStatus" =>'FAILED', "responseContent"=>'Error', "errorMessage" => $e->getMessage())));
+    $events[] = $event;
+  }
 }
 
+// Return the events as JSON response
+header('Content-Type: application/json'); // Set the content type header
+echo json_encode($events);
 ?>
