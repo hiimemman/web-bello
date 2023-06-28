84d2f088f0a4d7eb8ca89f0ea1c62ac4125074d5
Olarvs <mauuolarvs@gmail.com>
2023-06-28 21:21:50 +0800

    signout

--- web-bello/components/navbar.php
+++ web-bello/components/navbar.php
@@ -178,7 +178,7 @@ if (!isset($_SESSION['IDUSER'])) {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75">
                             </path>
                             </svg>    
-                                <span>Sign out</span>
+                                <span class="ml-3">Sign out</span>
                         </a>
                     </div>
                 </div>
