c9c24949ff0c596b46ea3b715147b1c15a61db54
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-28 22:53:32 +0800

    test

--- web-bello/components/navbar.php
+++ web-bello/components/navbar.php
@@ -39,8 +39,8 @@ if (!isset($_SESSION['IDUSER'])) {
 
 
   
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
 
    
     </head>
@@ -356,17 +356,8 @@ if (!isset($_SESSION['IDUSER'])) {
 
 
 <script defer>
-     
-// Initialize Toastr
-toastr.options = {
-  positionClass: 'toast-top-right', // Set the position of the notification
-  closeButton: true, // Show a close button
-  progressBar: true // Show a progress bar
-};
-
-// Show a notification
-toastr.success('Hello, Toastr!');
-
+     // Show a notification
+Swal.fire('Welcome back! <?php $result['firstname']?>');
 
     </script>
 </body>
