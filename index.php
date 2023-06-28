b8d21b26685cfed43773d64d00a32e66f297c27b
joegrayson <kingjoelunas@gmail.com>
2023-06-28 14:55:59 +0800

    Role selection page design test 48

--- index.php
+++ index.php
@@ -82,8 +82,8 @@ if (isset($_SESSION['IDUSER'])) {
 
     <section class="bg-gray-50 custom-bg">
         <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center ">
-            <div class="mx-auto max-w-xl text-center rounded-lg shadow bg-white px-16 py-20">
-                <h1 class="landing-content mb-4 text-4xl font-extrabold tracking-tight text-gray-800">
+            <div class="landing-content mx-auto max-w-xl text-center rounded-lg shadow bg-white px-16 py-20">
+                <h1 class=" mb-4 text-4xl font-extrabold tracking-tight text-gray-800">
                     Welcome to Web-Bello!
                 </h1>
 
@@ -114,52 +114,7 @@ if (isset($_SESSION['IDUSER'])) {
 
     <!-- End of New Landing Page -->
 
-    <!-- LANDING PAGE SECTION PLUS LOGIN FORM -->
 
-
-    <section class="bg-white dark:bg-gray-900 custom-bg">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h1
-                    class="landing-content max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-                    Welcome to Web-Bello!</h1>
-                <p
-                    class="landing-content max-w-2xl mb-6 font-medium text-white lg:mb-8 md:text-lg lg:text-xl dark:text-white">
-                    The
-                    ultimate hub for homeowners to stay connected and informed! With Web-Bello, you can easily connect
-                    with your neighbors, stay informed on the latest news and events, and receive important alerts and
-                    notifications.</p>
-
-                <!--<button style="color: white; class="p-4 rounded-lg landing-content">About Us</button>-->
-            </div>
-            <div class="landing-content lg:mt-0 lg:col-span-5 lg:flex">
-                <div
-                    class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1
-                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
-                        </h1>
-
-                        <div class="space-y-4 md:space-y-6">
-                            <a href="/web-bello/pages/user-login.php">
-                                <button type="submit" class="custom-signin-btn">
-                                    Click here
-                                </button>
-                            </a>
-
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Or <a href="/web-bello/pages/index.php" class="custom-text-btn">Log in</a> as
-                                administrator.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- END OF LANDING PAGE SECTION PLUS LOGIN FORM -->
 
 
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
