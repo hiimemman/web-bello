998b5f61a9386f902e83737dd40af8305d44f2f1
Olarvs <mauuolarvs@gmail.com>
2023-06-28 20:17:23 +0800

    Updating edit reservation admin

--- web-bello/pages/court-reservations.php
+++ web-bello/pages/court-reservations.php
@@ -260,7 +260,7 @@ th.sort-desc::after {
                         <input type="text" name="nameUpdate" id="nameUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                     </div>
 
-                    <div>
+                    <!-- <div>
                         <label for="startUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date-Time</label>
                         <input type="datetime-local" name="startUpdate" id="startUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
                     </div>
@@ -268,7 +268,7 @@ th.sort-desc::after {
                     <div>
                         <label for="endUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End Date-Time</label>
                         <input type="datetime-local" name="endUpdate" id="endUpdate"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                    </div>
+                    </div> -->
                 </div>
                 <div class="flex items-center">
                     <button type="submit" class="w-full focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">
