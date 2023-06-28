bb45f80493eca631809194d789b61a01f30ea07c
Olarvs <mauuolarvs@gmail.com>
2023-06-28 21:24:44 +0800

    new signout

--- web-bello/components/navbar.php
+++ web-bello/components/navbar.php
@@ -174,9 +174,9 @@ if (!isset($_SESSION['IDUSER'])) {
                             Manage dues
                         </a>
                         <a href="../api/login/logout.php" class="dropdown-item text-sm font-semibold leading-6 text-gray-900 dark:text-white block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-800">
-                            <span class="ml-3">Sign out</span>
                             <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75">
+                            Sign out
                             </path>
                             </svg>    
                             
