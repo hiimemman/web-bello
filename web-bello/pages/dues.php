1c151b49785301f8a2b44246931884c6072f234b
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-06-28 22:12:59 +0800

    test

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -272,7 +272,7 @@ th.sort-desc::after {
 
                     <div>
                         <label for="statusUpdate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
-                        <select id="statusUpdate" name ="statusUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <select id="statusUpdate" name ="statusUpdate"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option value="paid">Paid</option>
                             <option value="unpaid">Unpaid</option>
                         </select>
