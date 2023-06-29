0101bfa60f1768da05211e8863cc72ad71484b29
joegrayson <kingjoelunas@gmail.com>
2023-06-29 17:57:04 +0800

    Added mission vision statement section in homepage

--- web-bello/pages/user-index.php
+++ web-bello/pages/user-index.php
@@ -232,6 +232,77 @@ require_once('../components/navbar.php')
     </section>
     <!-- End of About Us Section -->
 
+    <!-- Mission Vision Statement Section -->
+
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="text-center mb-20">
+                <h1 class="sm:text-3xl title-font text-3xl font-bold text-gray-900 mb-4">
+                    Our Commitment: Mission & Vision
+                </h1>
+                <p class="leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500 tracking-wide text-md">
+                    Discover our unwavering commitment to excellence and our visionary
+                    goals that drive our homeowners association forward. Learn more
+                    about our mission and vision for a thriving community.
+                </p>
+                <div class="flex mt-6 justify-center">
+                    <div class="w-16 h-1 rounded-full bg-sky-900 inline-flex"></div>
+                </div>
+            </div>
+            <div class="flex flex-wrap -m-4">
+                <div class="p-4 md:w-1/2 w-full hover:shadow-xl transition-shadow">
+                    <div class="h-full bg-gray-100 p-8 rounded">
+                        <a class="inline-flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                                <path
+                                    d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z">
+                                </path>
+                            </svg>
+                            <span class="flex-grow flex flex-col pl-4 mb-4">
+                                <span class="title-font text-2xl font-medium text-gray-900">OUR MISSION</span>
+                            </span>
+                        </a>
+
+                        <p class="leading-relaxed mb-6 tracking-wide text-gray-700">
+                            Our mission is to provide a safe, inclusive, and welcoming
+                            environment where residents can enjoy a high quality of life. We
+                            aim to foster a strong sense of community, promote a healthy
+                            lifestyle, and provide top-notch amenities and services that
+                            enhance the well-being of our residents.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-4 md:w-1/2 w-full hover:shadow-xl transition-shadow">
+                    <div class="h-full bg-gray-100 p-8 rounded">
+                        <a class="inline-flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                                <path
+                                    d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z">
+                                </path>
+                            </svg>
+                            <span class="flex-grow flex flex-col pl-4 mb-4">
+                                <span class="title-font text-2xl font-medium text-gray-900">OUR VISION</span>
+                            </span>
+                        </a>
+
+                        <p class="leading-relaxed mb-6 tracking-wide text-gray-700">
+                            Our vision is to create a subdivision that sets the standard for
+                            exceptional living. We envision a community where neighbors come
+                            together to support and inspire each other, where children grow
+                            up in a nurturing environment, and where residents can enjoy a
+                            balanced and fulfilling lifestyle. We strive to be a model
+                            subdivision that others aspire to emulate.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- End of Mission Vision Statement Section -->
+
     <!-- Contact Us section -->
 
     <section class="bg-gray-200" id="contact">
@@ -334,20 +405,17 @@ require_once('../components/navbar.php')
     <!-- End of Footer-->
 
 
-    
-<script defer>
-// Show a notification
-Swal.fire({
-  title: 'Welcome back, <?php echo $result[0]['firstname']?>',
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 3000,
-  background: '#00ff00' // Green background color
-});
 
-
-
+    <script defer>
+    // Show a notification
+    Swal.fire({
+        title: 'Welcome back, <?php echo $result[0]['firstname']?>',
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        background: '#00ff00' // Green background color
+    });
     </script>
 </body>
 
