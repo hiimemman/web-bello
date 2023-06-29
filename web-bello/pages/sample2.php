17eec11c2a06adedbd768b7b53ecbc3394c15048
Olarvs <mauuolarvs@gmail.com>
2023-06-29 20:40:42 +0800

    required

--- web-bello/pages/sample2.php
+++ web-bello/pages/sample2.php
@@ -221,8 +221,9 @@ th.sort-desc::after {
                             <!--<p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>-->
     
                         </div>
+                    <div class="flex items-center">
                     <button type="submit" class="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">Reserve</button>
-                   
+                    </div>
                 </form>
             </div>
         </div>
