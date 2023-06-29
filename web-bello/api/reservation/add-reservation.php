4b9180b3df2bda68e8655a74a948cbf6d397586c
Olarvs <mauuolarvs@gmail.com>
2023-06-29 16:23:27 +0800

    fixing bugs

--- web-bello/api/reservation/add-reservation.php
+++ web-bello/api/reservation/add-reservation.php
@@ -5,16 +5,32 @@ header('Content-type: application/json');
 include_once("../../connections/connection.php");
 $con = connection();
 
-// Assuming you have a session storing the user's name
+// Assuming you have a session storing the user's ID
 session_start();
-$Name = $_SESSION['firstname'] . ' ' . $_SESSION['lastname'];
-
-
-$Title = $_POST['title'];
-$Start = $_POST['start_date'];
-$End = $_POST['end_date'];
+$userId = $_SESSION['IDUSER'];
 
 try {
+    // Fetch the user's first name and last name from tbl_residents
+    $sqlUser = "SELECT firstname, lastname FROM tbl_residents WHERE id = $userId";
+    $resultUser = mysqli_query($con, $sqlUser);
+    
+    if ($rowUser = mysqli_fetch_assoc($resultUser)) {
+        $firstName = $rowUser['firstname'];
+        $lastName = $rowUser['lastname'];
+        $Name = $firstName . ' ' . $lastName;
+    } else {
+        // Handle the case when user data is not found
+        exit(json_encode(array(
+            "responseStatus" => 'error',
+            "responseContent" => null,
+            "responseMessage" => 'User data not found!'
+        )));
+    }
+
+    $Title = $_POST['title'];
+    $Start = $_POST['start_date'];
+    $End = $_POST['end_date'];
+
     $sql = "INSERT INTO `tbl_reservation` (`title`, `reserved_by`, `start_date`, `end_date`) VALUES ('$Title', '$Name', '$Start', '$End')";
     mysqli_query($con, $sql);
 
