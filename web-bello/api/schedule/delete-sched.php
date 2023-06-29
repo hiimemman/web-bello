ce238ec7d6b95e37989a1638ed297f3f0668f2f1
Olarvs <mauuolarvs@gmail.com>
2023-06-30 00:26:46 +0800

    delete function for collection

--- web-bello/api/schedule/delete-sched.php
+++ web-bello/api/schedule/delete-sched.php
@@ -1,41 +1,27 @@
+
 <?php
-include_once("../../connections/connection.php");
-$con = connection();
+ header('Access-Control-Allow-Origin: *');
+ header('Content-type: application/json');
+ 
+ include_once("../../connections/connection.php");
+ $con = connection();
 
-// Delete event if requested
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_event'])) {
-    $eventID = $_POST['delete_event'];
-    $sql = "DELETE FROM tbl_sched WHERE id = $eventID";
-    if (mysqli_query($con, $sql)) {
-        echo json_encode(array('status' => 'success', 'message' => 'Event deleted successfully.'));
-    } else {
-        echo json_encode(array('status' => 'error', 'message' => 'Error deleting event.'));
-    }
-    exit();
+$ID = $_POST['IdDelete'];
+
+
+try{
+    
+
+
+    $sql = "DELETE FROM `tbl_sched` WHERE `tbl_sched`.`id` = '$ID';";
+    mysqli_query($con, $sql);
+
+
+    
+    exit(json_encode(array("responseStatus" =>'success', "responseContent" =>'reload', "responseMessage" =>'Reserved slot has been deleted!')));
+
+}catch(Exception $e){
+    exit(json_encode(array("responseStatus" =>'error', "responseContent" =>$e->getMessage(), "responseMessage" =>'Update failed error:!')));
 }
 
-// Select all events from the database
-$sql = "SELECT * FROM tbl_sched";
-$result = mysqli_query($con, $sql);
-
-// Check if any events were found
-if (mysqli_num_rows($result) > 0) {
-    $events = array();
-
-    // Iterate through each row and add it to the events array
-    while ($row = mysqli_fetch_assoc($result)) {
-        $event = array(
-            'id' => $row['id'],
-            'title' => $row['title'],
-            'start' => $row['start_date'],
-            'end' => $row['end_date']
-        );
-
-        $events[] = $event;
-    }
-}
-
-// Return the events as JSON response
-header('Content-Type: application/json'); // Set the content type header
-echo json_encode($events);
 ?>
