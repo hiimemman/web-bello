82d8d04f99f58305eff61da2a225f4c6da23a6ac
Olarvs <mauuolarvs@gmail.com>
2023-06-29 20:53:59 +0800

    designing payment user

--- web-bello/calendar-19/user-reservation.php
+++ web-bello/calendar-19/user-reservation.php
@@ -116,8 +116,8 @@ require_once('../components/navbar.php')
                         
       </div>
             <div>                               
-            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Payment Receipt</label>
-            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="image_url" type="file" required>
+            <label class="block mb-2" for="file_input">Payment Receipt</label>
+            <input class="block w-full" aria-describedby="file_input_help" id="image_url" type="file" required>
             <!--<p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>-->
             </div>
 
