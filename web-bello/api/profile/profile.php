8d2475833b61c8bd4fa3323667e7e7f43d9743a2
Olarvs <mauuolarvs@gmail.com>
2023-07-01 04:26:12 +0800

    Update profile pic

--- web-bello/api/profile/profile.php
+++ web-bello/api/profile/profile.php
@@ -36,30 +36,6 @@ if ($ID) {
             }
         }
 
-        // Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if a file is uploaded
-    if (isset($_FILES['image_url']) && $_FILES['image_url']['error'] === UPLOAD_ERR_OK) {
-        $image = $_FILES['image_url'];
-        $imagePath = 'https://web-bello.online/web-bello/savedimages/'; // Set the desired path to save the image
-        
-        // Move the uploaded image to the desired path
-        if (move_uploaded_file($image['tmp_name'], $imagePath)) {
-            // Update the database with the new image URL
-            $sql = "UPDATE tbl_residents SET image_url = '$imagePath' WHERE id = '$ID'";
-            $result = mysqli_query($con, $sql);
-
-            if ($result) {
-                // Image uploaded and database updated successfully
-                echo "Profile picture updated successfully.";
-            } else {
-                echo "Error updating profile picture: " . mysqli_error($con);
-            }
-        } else {
-            echo "Error moving uploaded image.";
-        }
-    }
-}
 
     }catch(Exception $e){
         exit(json_encode(array("responseStatus" =>'error', "responseContent" =>$e->getMessage(), "responseMessage" =>'Update failed error:!')));
