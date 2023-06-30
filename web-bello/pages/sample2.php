431fb7d2f66db27a92540f8b41f9817e00a4e5bf
Olarvs <mauuolarvs@gmail.com>
2023-07-01 06:41:10 +0800

    final

--- web-bello/pages/sample2.php
+++ web-bello/pages/sample2.php
@@ -120,7 +120,7 @@ require_once('../components/navbar.php')
                          </div>
 
                         <div class="mt-4">
-                            <label for="image_url" class="text-sm font-medium text-gray-700">Change Profile</label>
+                            <label for="image_url" class="text-sm font-medium text-gray-700">Change Profile Picture</label>
                             <input type="file" id="image_url" name="image_url" accept="image/*" class="mt-1">
                         </div>
                         </div>
