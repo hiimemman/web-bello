578cf8a54c6788c60f587411a1cfbb2762f77781
Olarvs <mauuolarvs@gmail.com>
2023-07-01 01:44:39 +0800

    changes in gender

--- web-bello/pages/resident-information.php
+++ web-bello/pages/resident-information.php
@@ -210,6 +210,7 @@ th.sort-desc::after {
                     <div>
                         <label for="sex" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sex</label>
                             <select id="sex" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                            <option selected>Choose</option>
                             <option value="Female">Female</option>
                             <option value="Male">Male</option>
                             </select>
