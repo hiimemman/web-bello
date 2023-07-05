92fb541fb127833eaa9e91b54923068b3005b13d
Olarvs <mauuolarvs@gmail.com>
2023-07-05 21:23:22 +0800

    hoa design

---
+++ web-bello/pages/users-hoa.php
@@ -0,0 +1,45 @@
+<?php
+require_once('../components/navbar.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div class="flex flex-col items-center justify-center">
+
+<div class="text-content">
+<img src="../savedimages/profile.png" alt="" srcset="">
+<h2 class="text-x1 font-bold">John Doe</h2>
+<p class="text-gray-500">President</p>
+</div>
+
+<div class="text-content">
+<img src="../savedimages/profile.png" alt="" srcset="">
+<h2 class="text-x1 font-bold">John Doe</h2>
+<p class="text-gray-500">President</p>
+</div>
+
+<div class="text-content">
+<img src="../savedimages/profile.png" alt="" srcset="">
+<h2 class="text-x1 font-bold">John Doe</h2>
+<p class="text-gray-500">President</p>
+</div>
+
+<div class="text-content">
+<img src="../savedimages/profile.png" alt="" srcset="">
+<h2 class="text-x1 font-bold">John Doe</h2>
+<p class="text-gray-500">President</p>
+</div>
+
+</div>
+
+
+</body>
+</html>
