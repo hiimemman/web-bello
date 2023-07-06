034520c23a61979cb073380004538287dc6c617b
Olarvs <mauuolarvs@gmail.com>
2023-07-06 22:08:45 +0800

    image only

--- web-bello/pages/court-reservations.php
+++ web-bello/pages/court-reservations.php
@@ -225,7 +225,7 @@ th.sort-desc::after {
                         <div>
                                                     
                             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Payment Receipt</label>
-                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="image_url" type="file" required>
+                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="image_url" accept="image/*" type="file" required>
                             <!--<p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>-->
     
                         </div>
