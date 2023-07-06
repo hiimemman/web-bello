3554071a884d8a110b6885d5053401038c6c0c90
Olarvs <mauuolarvs@gmail.com>
2023-07-06 21:18:50 +0800

    if users role

--- web-bello/pages/officials.php
+++ web-bello/pages/officials.php
@@ -145,7 +145,7 @@ const loadTable = async function(){
         if(users.status === 'Active'){
           user =  '<span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Active</span>'
         }
-        if(users.role !== 'admin'){
+        if(users.status === 'Active' && users.role !== 'admin'){
 
         content += `
         <div class="official">
