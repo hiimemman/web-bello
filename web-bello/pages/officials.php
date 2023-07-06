d1b24767baa27cbcfc1161e4f39da6fa41baa6e3
Olarvs <mauuolarvs@gmail.com>
2023-07-06 22:12:47 +0800

    officials title

--- web-bello/pages/officials.php
+++ web-bello/pages/officials.php
@@ -7,7 +7,7 @@ require_once('../components/navbar.php')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Palazzo Bello - Officials</title>
 </head>
 <body>
 
