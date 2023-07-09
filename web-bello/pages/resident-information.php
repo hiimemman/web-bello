c8c16bf709245eb79fe8cce9e1c653cd536edb3e
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-09 17:33:21 +0800

    test

--- web-bello/pages/resident-information.php
+++ web-bello/pages/resident-information.php
@@ -491,7 +491,7 @@ try{
 
 const response = await request.json();
 
-console.log("response: "+response)
+console.log("response: "+response.responseStatus)
     if(response.responseStatus === 'success'){
         //reload table
     location.reload();
