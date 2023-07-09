8e714c2dcbe3fbf2b83fb812b61e1f3b79dfec5f
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-09 19:18:17 +0800

    test

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -197,6 +197,11 @@ th.sort-desc::after {
             <div class="px-6 py-6 lg:px-8">
                 <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Generate new dues</h3>
                 <form class="space-y-6" action="#" id ="frmRegisterHOA">
+                    <div>
+                        <label for="amount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                        <input type="amount" name="amount" id="amount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
+                        <p class="mt-2 text-sm text-green-600 dark:text-green-500"><span class="font-medium">Well done!</span> Some success message.</p>
+                    </div>
                      <div>
                         <label for="month" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Month</label>
                             <select id="month" name ="month" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
