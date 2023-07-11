83d973af5dd8c1af571e22e1f174bf922f281aa2
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-11 14:24:20 +0800

    delte modal 2

--- web-bello/pages/hoa-official.php
+++ web-bello/pages/hoa-official.php
@@ -543,8 +543,8 @@ const deleteModal = async (button) =>{
     console.log(user)
     let  continueDelete = false
 
-    const deleteModalContent = `<div id="deleteModal" tabindex="-1" aria-hidden="false" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
-    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+    const deleteModalContent = `<div id="deleteModal" tabindex="-1" aria-hidden="false" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-modal md:h-full">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto bg-opacity-50 bg-black">
         <!-- Modal content -->
         <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
             <button type="button" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="deleteModal">
