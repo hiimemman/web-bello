bb46503f1fac9a43ae641986363491c3ccde1595
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-11 17:08:56 +0800

    tesT

--- web-bello/pages/dues.php
+++ web-bello/pages/dues.php
@@ -422,11 +422,11 @@ const loadTable = async function(){
                    Update
             </button>
              
-             <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-             id ="btnDelete`+contents.id+`"
-             data-user='`+JSON.stringify(contents)+`' onclick="deleteModal(this)">
-              Delete
-             </button>
+            //  <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+            //  id ="btnDelete`+contents.id+`"
+            //  data-user='`+JSON.stringify(contents.id)+`' onclick="deleteModal(this)">
+            //   Delete
+            //  </button>
         </div>                
         </div>
         </td>
