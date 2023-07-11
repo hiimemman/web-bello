32485a253fcecd2115356bfcef45630deb27b559
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-11 14:43:14 +0800

    test

--- web-bello/pages/hoa-official.php
+++ web-bello/pages/hoa-official.php
@@ -544,8 +544,8 @@ const deleteModal = async (button) =>{
     let  continueDelete = false
 
     const deleteModalContent = `
-    <div id="overlay" class="fixed top-0 right-0 bottom-0 left-0 bg-black opacity-100 z-50">
-    </div>
+    <div id="coverDiv" class="fixed inset-0 bg-black bg-opacity-50 z-50">
+  
     <div id="deleteModal" tabindex="-1" aria-hidden="false" class="fixed top-0 right-0 left-0 bottom-0 flex justify-center items-center">
     <div class="absolute top-0 right-0 bottom-0 left-0 bg-black bg-opacity-80"></div>
    
@@ -569,6 +569,7 @@ const deleteModal = async (button) =>{
         </div>
     </div>
     </div>
+</div>
 </div>
 `
 
