3a8ff03d47a071fc783d21069e34593e26bf708d
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-11 14:06:11 +0800

    delete modal update

--- web-bello/pages/hoa-official.php
+++ web-bello/pages/hoa-official.php
@@ -244,6 +244,13 @@ th.sort-desc::after {
     </div>
 </div> 
 
+<!-- DELETE MODAL -->
+
+
+    <!-- SHOW DELETE MODAL HERE -->
+    <div id ="divShowDeleteModalHere">
+
+    </div>
     
 
 <!-- UPDATE MODAL -->
@@ -318,13 +325,7 @@ th.sort-desc::after {
     </div>
 </div>
 
-<!-- DELETE MODAL -->
 
-
-    <!-- SHOW DELETE MODAL HERE -->
-    <div id ="divShowDeleteModalHere">
-
-    </div>
 
 <!-- HIDDEN BUTTON -->
 <button data-modal-target="updateProductModal" data-modal-toggle="updateProductModal" class="hidden block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" id ="updateHiddenButton">
