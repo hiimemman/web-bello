69c7fb440143053d337d6247a63cdb9772cc9905
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-12 12:31:13 +0800

    tset

--- web-bello/pages/court-reservations.php
+++ web-bello/pages/court-reservations.php
@@ -410,7 +410,7 @@ const loadTable = async function(){
         //by default inactive
         let user = '<span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">Inactive</span>'
 
-        const imageContent = ''
+        let imageContent = ''
 
         //change the badge color to active if active
         if(users.status === 'active'){
